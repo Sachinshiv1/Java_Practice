87724395c0131880d42af579f0109912fb736149
Sachin <sachinshivanand11111@gmail.com>
2022-12-14 11:14:15 +0530

    second commit

--- JavaInheritance/src/Main.java
+++ JavaInheritance/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        b.display1();
-        b.display2();
+//        b.display1();
+//        b.display2();
+        
+        inter AB = new inter();
+        AB.print();
+        AB.display();
     }
 }
 class A{
@@ -13,5 +17,20 @@ class A{
 class B extends A{
     void display2(){
         System.out.println("This is B");
+    }
+}
+
+interface multiInheritenceB{
+    void print();
+}
+interface multiInheritenceC{
+    void display();
+}
+class inter implements multiInheritenceB,multiInheritenceC{
+    public void print(){
+        System.out.println("This is interface A print");
+    }
+    public void display(){
+        System.out.println("This is interface B display");
     }
 }
